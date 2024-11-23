e047de2203b5b436755f09bccaaf5728574cd07a
Ratan Mia <ratan.kawasaki@gmail.com>
2024-11-23 17:49:12 +0600

    added Role Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Role;
 
 class User extends Authenticatable
 {
@@ -20,6 +22,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'company_id',
         'password',
     ];
 
@@ -44,5 +47,22 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($user) {
+            $accountantRole = Role::where('name', 'Accountant')->first();
+            $user->roles()->attach($accountantRole);
+        });
     }
 }
